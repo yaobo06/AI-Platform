90b292a2a9fd1fa42ea1b284a8015fe06baa7964
980144007 <980144007@qq.com>
2025-05-19 10:22:46 +0800

    内嵌模型对话

---
+++ src/views/AIApplication/modelConversation.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <div class="iframe-box">555</div> -->
+  <iframe class="iframe-box" src="http://192.168.16.75:3000/chat/share?shareId=5l9kbsry4igcaq5fyd1epxuy"></iframe>
+</template>
+<style lang="scss" scoped>
+.iframe-box {
+  width: 100%;
+  height: calc(100% - 16px);
+  margin-top: 8px;
+  border: none;
+  overflow: hidden;
+}
+</style>
