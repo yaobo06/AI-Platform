f720b9926bab1a7d2be53b3d4d364dcfff8a1e79
laichao <438380019@qq.com>
2025-05-20 15:34:57 +0800

    添加dify页面

---
+++ src/views/AIApplication/dify.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <div class="iframe-box">555</div> -->
+  <iframe class="iframe-box" src="http://192.168.16.67:81/apps"></iframe>
+</template>
+<style lang="scss" scoped>
+.iframe-box {
+  width: 100%;
+  height: calc(100% - 16px);
+  margin-top: 8px;
+  border: none;
+  overflow: hidden;
+}
+</style>
