bcf9757ca72f9fdcee859754939f309d1fe12602
980144007 <980144007@qq.com>
2025-06-05 10:18:50 +0800

    修复自带图片组件无法显示其它域名的图片问题

--- src/components/ImagePreview/index.vue
+++ src/components/ImagePreview/index.vue
@@ -12,8 +12,6 @@
 </template>
 
 <script>
-import { isExternal } from "@/utils/validate";
-
 export default {
   name: "ImagePreview",
   props: {
@@ -36,7 +34,7 @@ export default {
         return;
       }
       let real_src = this.src.split(",")[0];
-      if (isExternal(real_src)) {
+      if (/^http/.test(real_src)) {
         return real_src;
       }
       return process.env.VUE_APP_BASE_API + real_src;
@@ -48,7 +46,7 @@ export default {
       let real_src_list = this.src.split(",");
       let srcList = [];
       real_src_list.forEach(item => {
-        if (isExternal(item)) {
+        if (/^http/.test(item)) {
           return srcList.push(item);
         }
         return srcList.push(process.env.VUE_APP_BASE_API + item);
